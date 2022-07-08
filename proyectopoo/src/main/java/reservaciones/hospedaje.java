f4e607ef0ae52f16386ce36a5f5e4d33b8c70c15
Cesar Dominguez <cmdoming@espol.edu.ec>
2022-07-08 10:20:43 -0500

    se creo metodos de reserva

--- proyectopoo/src/main/java/reservaciones/hospedaje.java
+++ proyectopoo/src/main/java/reservaciones/hospedaje.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package reservaciones;
-
+import java.util.Scanner;
 /**
  *
  * @author LENOVO
@@ -11,10 +11,26 @@ package reservaciones;
 public class hospedaje extends servicio{
     protected String fechaEntr;
     protected String fechaSal;
-    
+    Scanner sc = new Scanner(System.in);
     @Override
     public String toString(){
         String a="si";
         return a;
+    }
+    public void reservar(){
+        System.out.println("/**********RESERVACION**********/\n/*                              /\n/*******************************/");
+    
+        System.out.print("Ingrese fecha de entrada (formato dd/mm/yy): ");
+        String fechaE = sc.nextLine();
+        System.out.print("Ingrese fecha de salida (formato dd/mm/yy): ");
+        String fechaS = sc.nextLine();
+        System.out.print("Que tipo de hospedaje busca?\n1. Hotel\n2.Departamento\nElija una opcion: ");
+        int reserva = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Ingrese el nombre de la ciudad donde se alojara: ");
+        String ciudad = sc.nextLine();
+        if(reserva==1){
+            
+        }
     }
 }
--- proyectopoo/src/main/java/reservaciones/servicio.java
+++ proyectopoo/src/main/java/reservaciones/servicio.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package reservaciones;
-
+import java.util.Scanner;
 /**
  *
  * @author LENOVO
@@ -13,11 +13,24 @@ public class servicio {
     protected String ciudad;
     protected double valor;
     protected double puntuacion;
+    Scanner sc = new Scanner(System.in);
     public servicio(int id_serv,String ciudad,double valor,double puntuacion){
         this.id_serv=id_serv;
         this.ciudad=ciudad;
         this.valor=valor;
         this.puntuacion=puntuacion;
+    }
+    
+    public void reservar(servicio serv){
+        System.out.println("/**********MENU**********/\n/*                      */\n/************************/");
+        System.out.print("1 Reservar hospedaje\n2. Reservar transporte\n3. Reservar entretenimiento\n4. Consultar reservas\n5. Salir");
+    }
+        }
+    public void pagarReserva(servicio serv){
+        
+    }
+    public String toString(){
+        return "";
     }
     
 }
