285b6a0b586012f1c4e3e106bf0011da137f00c1
RMDG2001 <68662586+RMDG2001@users.noreply.github.com>
2022-07-10 22:04:23 -0500

    Add files via upload

--- proyectopoo/src/main/java/Users/Administrador.java
+++ proyectopoo/src/main/java/Users/Administrador.java
@@ -4,10 +4,33 @@
  */
 package Users;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import reservaciones.servicio;
+
 /**
  *
  * @author ricar
  */
 public class Administrador {
-    
-}
+        
+    public void Acceder(){
+        for(String n: Usuario.lineas){
+            if(n.equals(nick)&& n.equals(pass)){
+                if(lineas.get(7).equals("A")){
+                    System.out.println("Bienvinido Administrador");
+                    System.out.println("1. COnsultar Reservas");
+                    System.out.println("2. Salir");
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Elija una opcion" + sc.nextInt());
+                    
+                    if(sc.equals(1)){
+                        System.out.println("-----"+ listaReservas[
+                                ]);
+                    }
+                }
+            } 
+        }    
+    }
+ }
